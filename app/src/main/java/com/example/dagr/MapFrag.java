424aeb7e1e0053e1e73af4fe74f201c81b9bc218
josephinehei <k.josephine.heinig@gmail.com>
2022-03-18 20:14:06 -0600

    started app

---
+++ app/src/main/java/com/example/dagr/MapFrag.java
@@ -0,0 +1,6 @@
+package com.example.dagr;
+
+import androidx.fragment.app.Fragment;
+
+public class MapFrag extends Fragment {
+}
---
+++ app/src/main/java/com/example/dagr/RadioFrag.java
@@ -0,0 +1,6 @@
+package com.example.dagr;
+
+import androidx.fragment.app.Fragment;
+
+public class RadioFrag extends Fragment {
+}
